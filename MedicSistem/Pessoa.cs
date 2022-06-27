b5da7737a22ced877598ac6fdac6a3e5b1f816e7
alissonpremium <alisson98sb@gmail.com>
2022-06-27 07:58:35 -0300

    Dev-02 Sistema de idendificacao medica

---
+++ MedicSistem/Pessoa.cs
@@ -0,0 +1,10 @@
+﻿namespace MedicSistem
+{
+    public class Pessoa
+    {
+        public string nome;
+        public int idade;
+        public double altura;
+        public double peso;
+    }
+}
---
+++ MedicSistem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MedicSistem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa();
+            pessoa1.nome = "Alisson Santana Barbosa";
+            pessoa1.idade = 24;
+            pessoa1.peso = 86;
+            pessoa1.altura = 1.80;
+
+            Console.WriteLine(pessoa1.nome);
+            Console.ReadKey();
+
+        }
+    }
+}
